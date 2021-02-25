ff74c05e12cc6640d7f02ec15df8d190f0cc8c7f
Woerner99 <woerner99@gmail.com>
2021-02-25 01:31:35 -0600

    user can save information to a file as <NAME> <BMR>

--- fit.py
+++ fit.py
@@ -6,7 +6,8 @@ Date: 02/04/2021
 
 """
 
-
+#Globals:
+save = 0
 
 
 def print_menu():
@@ -21,23 +22,23 @@ def print_menu():
 	print("(2) Calculate Calories Needed to Lose Weight")
 	print("(3) Calculate Calroies Needed to Gain Weight")
 	print("---------------------------------------------")
-	
-#This method uses the Harris-Benedict Formula	
+
+#This method uses the Harris-Benedict Formula
 
 def getBMR(gender, age, height, weight, bfp, act_level):
 
 	#Calculate Lean Mass (this will be used to calculate BMR)
 	weight = weight*(1-bfp)
-	
+
 	#Male
 	if(gender==1):
-		bmr = 66.47 + (6.24*weight) + (12.7*height) - (6.755*age)	
-	#Female	
+		bmr = 66.47 + (6.24*weight) + (12.7*height) - (6.755*age)
+	#Female
 	if(gender==2):
 		bmr = 655.1 + (4.35*weight) + (4.7*height) - (4.7*age)
 
 	return (float(bmr*act_level))
-	
+
 
 def personData():
 	gender = int(input("What is your gender?\n(1) Male\n(2) Female\n"))
@@ -51,46 +52,71 @@ def personData():
 	weight = float(input("What is your weight in pounds?\n"))
 	bfp = float(input("What is your body fat percentage? (For 20% = 0.2) \n"))
 	act_level = float(input("What is your activity level?\n(1.2) Sedentary\n(1.375) 1-3 days of exercise per week\n(1.55) 3-5 days of exercise per week\n(1.725) 6-7 days of exercise per week\n(1.9) Very hard exercise daily\n(x) Or enter a custom activity level\n"))
-	
-	
+
+
 	return gender,age,height,weight,bfp,act_level
 
 def printBMR():
 	gender,age,height,weight,bfp,act_level = personData()
 	bmr = getBMR(gender,age,height,weight,bfp,act_level)
-	
+
 	print("\nYour Calculated BMR is: "+str(bmr)+" calories burned per day.")
+	doWeSave(bmr)
 	back = input("Press ENTER to return...\n")
 
 def loseWeight():
 	gender,age,height,weight,bfp,act_level = personData()
 	bmr = getBMR(gender,age,height,weight,bfp,act_level)
-	
+
 	bmr = (bmr*.8)
 	print("\nYou need to consume: "+str(bmr)+" calroies per day in order to roughly lose 1lb of fat per week.")
+	doWeSave(bmr)
 	back = input("Press ENTER to return...\n")
 
 
 def gainWeight():
 	gender,age,height,weight,bfp,act_level = personData()
 	bmr = getBMR(gender,age,height,weight,bfp,act_level)
-	
+
 	bmr = (bmr*1.2)
 	print("\nYou need to consume: "+str(bmr)+" calroies per day in order to gain muscle.")
+	doWeSave(bmr)
 	back = input("Press ENTER to return...\n")
-	
+
+# Boolean function to determine whether or not to save the profile of user
+# 1 means save, else do nothing
+def doWeSave(bmr):
+	save = int(input("Would you like to save this entry?\n(1) YES\n(2) NO\n"))
+	if (save == 1):
+		name = input("Enter your name: ")
+		saveProfile(name, bmr)
+		print("Profile has been saved as user: " + name)
+
+# Save portfolio of to text file, writes person's name and BMR
+def saveProfile(name, bmr):
+	f = open("profiles.txt","a")
+	f.write(name+" "+str(bmr)+"\n")
+	f.close()
+
+"""
+	#TEST TO PRINT FILE
+	f = open("profiles.txt","r")
+	print(f.read())
+"""
+
 def sel():
-	choice = input()	
-	
+	choice = input()
+
 	if(choice == '1'):
-		printBMR()	
+		printBMR()
 	if(choice == '2'):
 		loseWeight()
 	if(choice == '3'):
 		gainWeight()
-		
-#==========================================		
-# MAIN	
+
+#==========================================
+# MAIN
+
 while(1):	
 	print_menu()	
 	sel()	
